6f652267961c1911119cde1814dcad0dbd926abe
Sebastian Lara Menares <slara@mtsolutions.io>
2020-04-21 09:59:11 -0400

    Add svg assets

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:mtfifo/models/storageunit.dart';
 import 'package:timeago/timeago.dart' as timeago;
+import 'package:flutter_svg/flutter_svg.dart';
 
 
 final List<String> entries = <String>['A'];
